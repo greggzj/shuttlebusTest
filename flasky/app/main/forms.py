c2af9901680c5f47268740d4ffdb5ad60a4d5a06
shuttle bus HW <597299901@qq.com>
2017-12-13 16:52:14 +0800

    adding files

---
+++ flasky/app/main/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import Form
+from wtforms import StringField, SubmitField, BooleanField, TextAreaField, SelectField
+from wtforms.validators import Required, Length, Email, Regexp
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class EditProfileForm(Form):
+    name = StringField('Real name', validators=[Length(0,64)])
+    location = StringField('Location', validators=[Length(0,64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
+class EditProfileAdminForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                             Email()])
+    username = StringField('Username', validators=[
+        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                          'Usernames must have only letters, '
+                                          'numbers, dots or underscores')])
+    confirmed = BooleanField('confirmed')
+    role = SelectField('Role', coerce=int)
---
+++ flasky/app/main/views.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+from flask import render_template, session, redirect, url_for
+
+from . import main
+from .forms import NameForm, EditProfileForm
+from .. import db
+from ..models import User
+from flask_login import login_required, current_user
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known']=False
+        else:
+            session['known']=True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('.index'))
+    return render_template('index.html',
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False),
+                           current_time=datetime.utcnow())
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html', user=user)
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.about_me.data = current_user.about_me
+    form.location = current_user.location
+    return render_template('edit_profile.html', form=form)
